1d483a98beb3ff74666ee6253ff0e8b6eb600770
dnttr <147146363+dnttr@users.noreply.github.com>
2025-05-10 00:19:48 +0200

    Enhance MethodProxy: add move semantics, delete copy constructor, and mark build_identity as nodiscard

--- cpp/include/jni/proxies/MethodProxy.hpp
+++ cpp/include/jni/proxies/MethodProxy.hpp
@@ -23,7 +23,7 @@ class MethodProxy
     std::vector<std::string> parameters;
     bool is_static;
 
-    jmethodID build_identity() const
+    [[nodiscard]] jmethodID build_identity() const
     {
         const auto identity = is_static ? env->GetStaticMethodID(owner->get_klass(), name.c_str(), signature.c_str())
                                         : env->GetMethodID(owner->get_klass(), name.c_str(), signature.c_str());
@@ -38,14 +38,46 @@ class MethodProxy
 public:
 
     MethodProxy(JNIEnv *env, std::unique_ptr<KlassProxy> owner, std::string &name, std::string &signature, std::vector<std::string> parameters, const bool is_static)
-    :
-    env(env),
-    owner(std::move(owner)),
-    name(std::move(name)),
-    signature(std::move(signature)),
-    parameters(std::move(parameters)),
-    is_static(is_static)
+        :
+        env(env),
+        owner(std::move(owner)),
+        name(std::move(name)),
+        signature(std::move(signature)),
+        parameters(std::move(parameters)),
+        is_static(is_static)
     {
         identity = build_identity();
     }
+
+    explicit MethodProxy(nullptr_t) = delete;
+
+    MethodProxy(const MethodProxy &) = delete;
+    MethodProxy &operator=(const MethodProxy &) = delete;
+
+    MethodProxy(MethodProxy &&other) noexcept
+        :
+        env(other.env),
+        identity(other.identity),
+        owner(std::move(other.owner)),
+        name(std::move(other.name)),
+        signature(std::move(other.signature)),
+        parameters(std::move(other.parameters)),
+        is_static(other.is_static)
+    {
+    }
+
+    MethodProxy &operator=(MethodProxy &&other) noexcept
+    {
+        if (this != &other)
+        {
+            identity = other.identity;
+            owner = std::move(other.owner);
+            name = std::move(other.name);
+            signature = std::move(other.signature);
+            parameters = std::move(other.parameters);
+            is_static = other.is_static;
+        }
+        return *this;
+    }
+
 };
