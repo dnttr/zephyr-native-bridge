276e6bad867fd5d0a55f88eee7bb192bd5b2fea3
dnttr <147146363+dnttr@users.noreply.github.com>
2025-05-25 23:01:33 +0200

    Add buffer management methods: implement set_ptr and get_ptr for JNIEnv buffer handling

--- cpp/main/include/ZNBKit/jni/signatures/method/byte_method.hpp
+++ cpp/main/include/ZNBKit/jni/signatures/method/byte_method.hpp
@@ -3,3 +3,22 @@
 //
 
 #pragma once
+
+#include "ZNBKit/jni/signatures/method_signature.hpp"
+
+namespace znb_kit
+{
+    class byte_method final : public method_signature<jbyte>
+    {
+    public:
+        using method_signature::method_signature;
+
+        jbyte invoke(const jobject &instance, std::vector<jvalue> &parameters) override {
+            if (is_static) {
+                return env->CallStaticByteMethod(get_owner(), get_identity(), parameters.data());
+            }
+
+            return env->CallByteMethod(instance, get_identity(), parameters.data());
+        }
+    };
+}
--- cpp/main/include/ZNBKit/jni/signatures/method/long_method.cpp
+++ cpp/main/include/ZNBKit/jni/signatures/method/long_method.cpp
@@ -3,3 +3,22 @@
 //
 
 #pragma once
+
+#include "ZNBKit/jni/signatures/method_signature.hpp"
+
+namespace znb_kit
+{
+    class long_method final : public method_signature<jlong>
+    {
+    public:
+        using method_signature::method_signature;
+
+        jlong invoke(const jobject &instance, std::vector<jvalue> &parameters) override {
+            if (is_static) {
+                return env->CallStaticLongMethod(get_owner(), get_identity(), parameters.data());
+            }
+
+            return env->CallLongMethod(instance, get_identity(), parameters.data());
+        }
+    };
+}
