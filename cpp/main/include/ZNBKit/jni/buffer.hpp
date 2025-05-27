7e28723034a55be0b99a4d29ab73d29946ffe536
dnttr <147146363+dnttr@users.noreply.github.com>
2025-05-27 23:40:45 +0200

    Add buffer methods: implement set_ptr_long, get_ptr_long, set_ptr_byte, and get_ptr_byte for improved JNIEnv buffer handling

--- cpp/main/include/ZNBKit/jni/buffer.hpp
+++ cpp/main/include/ZNBKit/jni/buffer.hpp
@@ -9,11 +9,19 @@
 #include <stdexcept>
 #include <utility>
 
-class buffer
+namespace znb_kit
 {
-public:
+    class buffer
+    {
+        static int get_length(JNIEnv *env, const jarray &buffer, int array_length, int buffer_offset = 0);
+    public:
 
-    static jint set_ptr(JNIEnv *env, const jlongArray &buffer, const uint32_t *ptr, const size_t *size);
+        static int set_ptr_long(JNIEnv *env, const jlongArray &buffer, const jlong *array, int array_length, int buffer_offset = 0);
 
-    static jint get_ptr(JNIEnv *env, const jlongArray &buffer, std::pair<uint32_t, size_t> &addr);
-};
+        static int get_ptr_long(JNIEnv *env, const jlongArray &buffer, jlong *array, int array_length, int buffer_offset = 0);
+
+        static int set_ptr_byte(JNIEnv *env, const jbyteArray &buffer, const int8_t *array, int array_length, int buffer_offset = 0);
+
+        static int get_ptr_byte(JNIEnv *env, const jbyteArray &buffer, int8_t *array, int array_length, int buffer_offset = 0);
+    };
+}
